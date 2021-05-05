ad2fbbf62753eb5aff5ecf6cbf8685c06f9605a9
najibjodiansyah <najibjodiansyah11@gmail.com>
2021-05-05 19:10:51 +0700

    implementasi redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {increment,decrement} from './redux/actions/counter';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>point increment and decrement aplication using redux</p>
+        <button onClick={() => dispatch(increment())}>Plus one</button>
+        <button onClick={() => dispatch(decrement())}>Minus one</button>
+        nilai saat ini: {counter.value}
       </header>
     </div>
   );
